42158351fe7845de5e89b6412470e991a6d4a96a
Grant Gumina <grantgumina@outlook.com>
2017-10-17 07:42:07 -0700

    more exercises

--- src/koans/08_conditionals.clj
+++ src/koans/08_conditionals.clj
@@ -10,28 +10,26 @@
 
 (meditations
   "You will face many decisions"
-  (= __ (if (false? (= 4 5))
-          :a
-          :b))
+  (= :a (if (false? (= 4 5)) :a :b)) ;; if true then a, otherwise b
 
   "Some of them leave you no alternative"
-  (= __ (if (> 4 3)
+  (= [] (if (> 4 3)
           []))
 
   "And in such a situation you may have nothing"
-  (= __ (if (nil? 0)
+  (= nil (if (nil? 0)
           [:a :b :c]))
 
   "In others your alternative may be interesting"
   (= :glory (if (not (empty? ()))
               :doom
-              __))
+              :glory))
 
   "You may have a multitude of possible paths"
   (let [x 5]
-    (= :your-road (cond (= x __) :road-not-taken
-                        (= x __) :another-road-not-taken
-                        :else __)))
+    (= :your-road (cond (= x 10) :road-not-taken
+                        (= x 3) :another-road-not-taken
+                        :else :your-road)))
 
   "Or your fate may be sealed"
   (= 'doom (if-not (zero? __)
